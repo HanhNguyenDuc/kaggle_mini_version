5340d36c43b9dcd219520b339b5590ec8cf01dad
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2020-02-24 15:38:17 +0700

    change root submit-app from _home_ to __

--- kaggle_instance/settings.py
+++ kaggle_instance/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT   =   os.path.join(os.getcwd())
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
